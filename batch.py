b36ad3f7ce121f00aa19d6ea85b2c9140dd2cb1f
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2017-11-08 20:32:24 +0100

    add residue

--- batch.py
+++ batch.py
@@ -127,3 +127,35 @@ def ana(dens_type=0,ishell=5):
 
 
     return clrec-clt
+
+
+def residues(ishell=5,lmax=500):
+
+    ishell=5
+    truth="clR4_shell{:0d}_bordersout.txt".format(ishell)
+    
+    lt,clt=loadtxt(truth,unpack=True)
+    clt[0]=0
+
+
+
+    dens=("LogN","1LPT","2LPT","Gaussclip")
+
+    i=0
+    for dens_type in dens :
+        f1=os.path.join("batch","dens_type{}".format(i),"shell{}".format(ishell),"clmean.fits")
+        clrec=hp.read_cl(f1)
+        l=arange(len(clrec))
+        res=clrec-clt[l]
+        plot(res,label=dens_type,alpha=0.7)
+        i+=1
+        
+    plot(clt[l]/100,'k--',label=r"$C_\ell^{true}/100$")
+    legend()
+    axhline(0,color='k',lw=0.5)
+    xlabel(r"$\ell$")
+    ylabel(r"residue")
+    xlim(0,lmax)
+    tight_layout()
+    show()
+
