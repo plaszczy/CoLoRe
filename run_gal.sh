61f71eacc83550b51abd2338c6444807823d3805
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2017-11-14 21:15:29 +0100

    add ngrid

--- run_gal.sh
+++ run_gal.sh
@@ -2,9 +2,10 @@
 NSIM=1
 
 dens_type=0
+ngrid=400
 
 TEMPLATE="template_gal.cfg"
-OUTDIR="batch/dens_type${dens_type}"
+OUTDIR="batch/ngrid$ngrid/dens_type${dens_type}"
 mkdir -p $OUTDIR
 
 #loop
@@ -13,12 +14,12 @@ for ((i=0 ; i<$NSIM ; i++))
 do
 seed=$RANDOM
  
-awk -v seed=$RANDOM -v dens=$dens_type '{if (/_SEED_/) {print "seed = "seed}  else if (/_DENS_TYPE_/) {print "dens_type="dens} else {print}}' template_gal.cfg > tt.cfg
+awk -v seed=$RANDOM -v dens=$dens_type -v ngrid=$ngrid '{if (/_SEED_/) {print "seed = "seed}  else if (/_DENS_TYPE_/) {print "dens_type="dens} else if (/_N_GRID_/) {print "n_grid="ngrid} else {print}}' template_gal.cfg > tt.cfg
 
 ./CoLoRe tt.cfg
 
 #renommer
-mv -f tt.cfg $OUTDIR/cat$seed.cfg
+cp tt.cfg $OUTDIR/cat$seed.cfg
 mv -f out_srcs_0.fits $OUTDIR/cat$seed.fits
-#rm -f out*
+rm -f out*
 done
