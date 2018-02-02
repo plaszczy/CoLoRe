4aa4248ef2cd80e600e92c834a74c3c02ab013cb
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-02-02 16:38:38 +0100

    segfault

--- HEAD/src/Window.hh
+++ HEAD/src/Window.hh
@@ -30,7 +30,7 @@ public:
   inline double zmax() const { return _zmax;}
   inline bool in(const double& z) const {return z>=_zmin && z<=_zmax;}
 
-  virtual double weight(const double& z)=0;
+  virtual double weight(const double& z) const =0;
 
 
 private:
@@ -42,14 +42,14 @@ class UniformWindow : public Window
 {
 public:
   UniformWindow(double zmin,double zmax):Window(zmin,zmax){}
-  inline double weight(const double& z){return 1;}
+  inline double weight(const double& z) const {return 1;}
 };
 
 class GaussWindow : public Window 
 {
 public:
   GaussWindow(double mean,double sigma,double nsigcut=3):Window(mean-nsigcut*sigma,mean+nsigcut*sigma),_zmean(mean),_sigma(sigma){}
-  inline double weight(const double& z){return std::exp(std::pow((z-_zmean)/_sigma,2)/2);}
+  inline double weight(const double& z) const {return std::exp(std::pow((z-_zmean)/_sigma,2)/2);}
 private:
   double _zmean,_sigma;
 };
--- HEAD/src/proj.cc
+++ HEAD/src/proj.cc
@@ -9,6 +9,7 @@
 #include "arr.h"
 #include "fitshandle.h"
 #include "powspec_fitsio.h"
+#include"lsconstants.h"
 #include "announce.h"
 
 #include<iostream>
@@ -63,9 +64,27 @@ public:
 
   inline void fillIndex(const size_t& i) {index.push_back(i);}
 
+  void projectMap(int nside){
+    map.SetNside(nside,RING); 
+    //loop on gals
+    for (auto i : index) {
+      size_t igal=index[i];
+      pointing p(degr2rad*(90.-(cat.dec)[igal]),degr2rad*(cat.ra)[igal]);
+      long ipix=map.ang2pix(p);
+      map[ipix]+=win->weight(cat.z[igal]);
+    }
+  }
+  void writeMap(){
+    stringstream os;
+    os << "mapgal_"<< win->zmin() << "-" << win->zmax() <<".fits";
+    write_Healpix_map_to_fits(os.str(),map,PLANCK_FLOAT32);
+  }
+
   vector<size_t> index;
   const Catalog& cat;
   const Window* win;
+  Healpix_Map<double> map;
+
 
 };
 
@@ -75,7 +94,8 @@ auto main(int argc,char** argv)-> int {
   announce(argv[0]);
   vector<double> zmean={0.15,0.25,0.35,0.45};
   double width=0.05;
-  
+  const int nside=512;
+
   bool rsd=true;
 
   try{
@@ -89,18 +109,19 @@ auto main(int argc,char** argv)-> int {
       shell.push_back(Shell(cat,new UniformWindow(zmean[i]-width,zmean[i]+width)));
 
 
-    //fill index looping once on the catalog
+    //fill shell index looping once on the catalog
     for (size_t i=0;i<cat.z.size();i++){
       double z=cat.z[i];
       for (auto &s : shell)
 	if (s.win->in(z)) s.fillIndex(i);
     }
-
     
-    for (auto s : shell)
+    //shell loop
+    for (auto s : shell){
       cout <<" shell [" << s.win->zmin() <<"," <<  s.win->zmax() << "]: " << s.index.size() << " gals" << endl;
-
-    
+      s.projectMap(nside);
+      s.writeMap();
+    }
 
 
 
