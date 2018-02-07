10f11f61748a42f902485187fae43e8a13546ef2
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-02-07 23:45:03 +0100

    fix

--- HEAD/cmt/run_proj.sh
+++ HEAD/cmt/run_proj.sh
@@ -1,8 +1,12 @@
+
+file=$1
+
 grep -v file tophat4.par > tmp.par
-filein=$1
-echo "filein=$filein" >> tmp.par
-dir=$(dirname $filein)
-f=$(basename $filein)
+
+grep -v file tophat4.par > tmp.par
+echo "filein=$file" >> tmp.par
+dir=$(dirname $file)
+f=$(basename $file)
 fileout="!"$dir/cls_${f}
 echo "fileout=$fileout" >> tmp.par
 ../$CMTCONFIG/proj tmp.par
