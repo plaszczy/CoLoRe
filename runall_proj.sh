4d84292be63b8bc42d07cfbbfe664172dee44bbc
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-02-08 11:28:09 +0100

    ameliore utilisation script

--- runall_proj.sh
+++ runall_proj.sh
@@ -1,8 +1,31 @@
+
+nargs=$#
+if ! [ $nargs -eq 2 ]; then
+echo "usage: proj_all.sh dir(cat*.fits) config.par -> dir/cls_cat*"
+exit 1
+fi
+
 dir=$1
+conf=$2
 
-for filein in $dir/*.fits ; do
+ls $dir/cat*.fits
+n=`ls -1  $dir/cat*.fits | wc -l`
 
-grep -v file HEAD/cmt/tophat4.par > tmp.par
+echo "$n files : OK? [y/n]"
+read answer
+
+if [ $answer != 'y' ] ; then
+exit 1
+fi
+
+
+if ! [ $? -eq 0 ] ; then
+exit
+fi
+
+for filein in $dir/cat*.fits ; do
+
+grep -v file $conf > tmp.par
 echo "filein=$filein" >> tmp.par
 dir=$(dirname $filein)
 f=$(basename $filein)
