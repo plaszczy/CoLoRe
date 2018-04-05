5c66010597a86d3fcecd9a875638904ce93d6948
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-04-05 22:17:07 +0200

    add hdf5 to gcc

--- prodnew.py
+++ prodnew.py
@@ -6,38 +6,44 @@ def ratio(t,truth,h,lmax=200):
     for n in names[1:] :
         cl=t[n][1:lmax]
         if h['REMOVESN']==False:
-            SN=mean(t[n][500:])
+            SN=mean(t[n][600:])
             print("estimating SN for {}={}".format(n,SN))
-            #cl-=SN
-        plot(t.ell[1:lmax],cl,label=n)
-        #plot(t.ell[1:lmax],cl/truth[n][1:lmax],label=n)
+            cl-=SN
+        #plot(t.ell[1:lmax],cl,label=n)
+        plot(t.ell[1:lmax],cl/truth[n][1:lmax],label=n)
         xlabel(r"$\ell$")
-        ylabel(r"$C_\ell(rec)/C_\ell(true)$")
+        ylabel(r"$<C_\ell(rec)>/C_\ell(true)$")
         legend()
-        #ylim(0,3)
-        #ax1()
-        ax0()
+        ylim(0.8,1.2)
+        ax1()
+        #ax0()
 
-#
-#data="outputs/bench3/clmean.fits"
-#model="model/tophat_clip120.fits"
+#LOGN
 
-data="outputs/tophatsmear_logn/clmean.fits"
-data="outputs/gauss4_dens0/clmean.fits"
-model="model/clgauss_logn.fits"
+#tophat
+#data="outputs/bench0/clmean.fits"
+#model="model/cltophat_logn.fits"
+#gauss
+#data="outputs/gauss4_dens0/clmean.fits"
+#model="model/clgauss_logn.fits"
 
-#data="outputs/tophatsmear_clip/clmean.fits"
-#model="model/clgauss_clip.fits"
+###CLIP
+###tophat
+##data="outputs/bench3/clmean.fits"
+##model="model/cltophat_clip.fits"
+#gauss
+data="outputs/gauss4_dens3/clmean.fits"
+model="model/clgauss_clip.fits"
 
-
+#SMEARED
 #data="outputs/tophat4/clmean.fits"
 #model="model/tophat_logn120.fits"
 
-truth=mrdfits(model,1)
+tmod=mrdfits(model,1)
 (t,h)=mrdfits(data,1,header=True)
 
 
 #figure()
-ratio(t,truth,h,200)
+ratio(t,tmod,h,200)
 title(model)
 tight_layout()
