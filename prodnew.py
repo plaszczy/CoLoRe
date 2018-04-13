745e3d43abc019993e2e3e3488694465527f5afe
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-04-13 23:16:59 +0200

    updated note

--- prodnew.py
+++ prodnew.py
@@ -23,17 +23,20 @@ def ratio(t,truth,h,lmax=200):
 #tophat
 #data="outputs/bench0/clmean.fits"
 #model="model/cltophat_logn.fits"
+#
+#data="outputs/bench0/clmean_norsd.fits"
+#model="model/cltophat_logn_norsd.fits"
 #gauss
 #data="outputs/gauss4_dens0/clmean.fits"
 #model="model/clgauss_logn.fits"
 
 ###CLIP
 ###tophat
-##data="outputs/bench3/clmean.fits"
-##model="model/cltophat_clip.fits"
+data="outputs/bench3/clmean.fits"
+model="model/cltophat_clip.fits"
 #gauss
-data="outputs/gauss4_dens3/clmean.fits"
-model="model/clgauss_clip.fits"
+#data="outputs/gauss4_dens3/clmean.fits"
+#model="model/clgauss_clip.fits"
 
 #SMEARED
 #data="outputs/tophat4/clmean.fits"
@@ -43,7 +46,7 @@ tmod=mrdfits(model,1)
 (t,h)=mrdfits(data,1,header=True)
 
 
-#figure()
+figure()
 ratio(t,tmod,h,200)
 title(model)
 tight_layout()
