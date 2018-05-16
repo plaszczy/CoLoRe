1e051b13dfef8adf8c39452041e2a7f53772a0ad
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-05-16 21:00:17 +0200

    note update

--- prodnew.py
+++ prodnew.py
@@ -5,18 +5,18 @@ def ratio(t,truth,h,lmax=200):
     names=t.names
     for n in names[1:] :
         cl=t[n][1:lmax]
-        #if h['REMOVESN']==False:
-        #    SN=mean(t[n][600:])
-        #    print("estimating SN for {}={}".format(n,SN))
-        #    cl-=SN
-        plot(t.ell[1:lmax],cl,label=n)
-        #plot(t.ell[1:lmax],cl/truth[n][1:lmax],label=n)
+        if h['REMOVESN']==False:
+            SN=mean(t[n][600:])
+            print("estimating SN for {}={}".format(n,SN))
+            cl-=SN
+        #plot(t.ell[1:lmax],cl,label=n)
+        plot(t.ell[1:lmax],cl/truth[n][1:lmax],label=n)
         xlabel(r"$\ell$")
         ylabel(r"$<C_\ell(rec)>/C_\ell(true)$")
         legend()
-        #ylim(0.8,1.2)
-        #ax1()
-        ax0()
+        ylim(0.8,1.2)
+        ax1()
+        #ax0()
 
 #LOGN
 #tophat
@@ -26,13 +26,13 @@ def ratio(t,truth,h,lmax=200):
 #data="outputs/bench0/clmean_norsd.fits"
 #model="model/cltophat_logn_norsd.fits"
 #gauss
-data="outputs/gauss4_dens0/clmean.fits"
-model="model/clgauss_logn.fits"
+#data="outputs/gauss4_dens0/clmean.fits"
+#model="model/clgauss_logn.fits"
 
 ###CLIP
 ###tophat
-#data="outputs/bench3/clmean.fits"
-#model="model/cltophat_clip.fits"
+data="outputs/bench3/clmean.fits"
+model="model/cltophat_clip.fits"
 #gauss
 #data="outputs/gauss4_dens3/clmean.fits"
 #model="model/clgauss_clip.fits"
@@ -45,6 +45,6 @@ tmod=mrdfits(model,1)
 (t,h)=mrdfits(data,1,header=True)
 
 figure()
-ratio(t,tmod,h,700)
+ratio(t,tmod,h,200)
 title(model)
 tight_layout()
--- selfun.py
+++ selfun.py
@@ -1,46 +1,58 @@
+from pylab import *
 
 from tools import *
 
-
-i=1
-
-#dndz=5000
-t=mrdfits("outputs/bench0/clmean.fits",1)
-
-#red
-tr,hr=mrdfits("outputs/bench0/clmean_red.fits",1,header=True)
-
-colnames=tr.names
-n=colnames[i+1]
-
-cl1=t[n]
-
-cl2=tr[n]
-#remove SN
-S2=mean(cl2[600:])
-cl2-=S2
-
-figure()
-subplot(2,1,1)
-
-title(n)
-plot(cl1,label='const')
-plot(cl2,'r',label='red')
-xlim(1,200)
-legend()
-ax0()
-ylabel("Cl")
-
-rcl=cl2/cl1
-
-subplot(2,1,2)
-plot(rcl[1:200],'r')
-xlim(1,200)
-ax1()
-ylabel("Cl(red)/Cl(const)")
+import sys
 
 
-#SHOTNOISE
-Ntot=hr["ngal{}".format(i)]
+def dndz_spectra(cl1,cl2):
+        #figure()
+        subplot(2,1,1)
+        
+        title(n)
+        plot(cl1,label='const')
+        plot(cl2,'r',label='red')
+        xlim(1,200)
+        legend()
+        ax0()
+        ylabel("Cl")
+        rcl=cl2-cl1
+        subplot(2,1,2)
+        plot(rcl[1:200],'r')
+        xlim(1,200)
+        ax0()
+        ylabel("Cl(red)-Cl(const)")
+        
+        tight_layout()
 
-print("||{:04.2e} || {:04.2e} || {:04.2e} ||".format(4*pi/Ntot,S2,S2/(4*pi/Ntot)))
+def angpow(t,ta,h,lmax=500):
+    figure()
+    it=gca()._get_lines.prop_cycler
+    names=t.names
+    for n in names[1:] :
+        cl=t[n][1:lmax]
+        cla=ta[n][1:lmax]
+        if h['REMOVESN']==False:
+            Ntot=h["ngal"+n[-1]]
+            sn=4*pi/Ntot
+            cl-=sn
+        col=next(it)['color']
+        plot(cl,label=n,color=col)
+        plot(cla,'--',color=col)
+        plot(cl-cla,color=col)
+        ax0()
+    legend()
+
+#data
+t,h=mrdfits("outputs/bench0/clmean_red.fits",1,header=True)
+ta=mrdfits("model/cltophat_logn_red.fits",1)
+
+#t,h=mrdfits("outputs/bench0/clmean.fits",1,header=True)
+#ta=mrdfits("model/cltophat_logn.fits",1)
+
+#
+
+angpow(t,ta,h)
+
+
+#print("||{:04.2e} || {:04.2e} || {:04.2e} ||".format(4*pi/Ntot,S2,S2/(4*pi/Ntot)))
